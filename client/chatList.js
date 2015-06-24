8e97122eaf649149346b4871208d80fc284d6173
Peter Rogers <progers@surgerforward.com>
2015-06-23 19:00:06 -0500

    Add publish/subscribe.

--- client/chatList.js
+++ client/chatList.js
@@ -1,9 +1,5 @@
 Template.chatList.helpers({
     sampleChats: function() {
-        return [
-            {text: "Hi!"},
-            {text: "How are you?"},
-            {text: "'sup!"}
-        ];
+        return Chats.find();
     }
 });
